8f3a4c9d528c865d949c71f0d0534054bb4a7b21
JayMgitM <mowohbaar@yahoo.com>
2024-09-02 20:41:07 -0500

    update

--- src/main/java/com/baar/springbootsqlserver/SpringbootSqlserverApplication.java
+++ src/main/java/com/baar/springbootsqlserver/SpringbootSqlserverApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-//@EnableAutoConfiguration(
-//		exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
 @RestController
 public class SpringbootSqlserverApplication {
